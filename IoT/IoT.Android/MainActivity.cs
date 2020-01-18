4170d1677f606c9f20b9cc5e1410ac07d546faa1
Mark Griffiths <mark.griffiths68@btopenworld.com>
2020-01-18 13:49:20 +0000

    Add project files.

---
+++ IoT/IoT.Android/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace IoT.Droid
+{
+    [Activity(Label = "IoT", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+
+            SetContentView(Resource.Layout.layout1);
+
+            Button button = FindViewById<Button>(Resource.Id.button1);
+            button.Click += Button1Clicked;
+                       
+        }
+
+        private void Button1Clicked(object sender, EventArgs eventArgs)
+        {
+            TextView tv = FindViewById<TextView>(Resource.Id.textView1);
+            tv.Text = "OK";
+        }
+        
+    }
+}
